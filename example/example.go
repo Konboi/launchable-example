318df7a7846f0388c17ef9e5ea0e125c8981697e
Konboi <ryosuke.yabuki@gmail.com>
2023-04-27 17:18:02 +0900

    add test

--- example/example.go
+++ example/example.go
@@ -5,3 +5,19 @@ type Example struct{}
 func (e *Example) Greeting() string {
 	return "Hello"
 }
+
+func (e *Example) Add(a, b int) int {
+	return a + b
+}
+
+func (e *Example) Sub(a, b int) int {
+	return a - b
+}
+
+func (e *Example) Mul(a, b int) int {
+	return a * b
+}
+
+func (e *Example) Div(a, b int) int {
+	return a / b
+}
