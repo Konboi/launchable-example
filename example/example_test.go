256618aa95cd1ade41a48a764aae33c05f3b63b5
Konboi <ryosuke.yabuki@gmail.com>
2023-04-27 21:29:23 +0900

    add test

--- example/example_test.go
+++ example/example_test.go
@@ -40,3 +40,31 @@ func TestExampleAdd(t *testing.T) {
 		})
 	}
 }
+
+func TestExampleDiv(t *testing.T) {
+	e := &example.Example{}
+
+	tests := map[string]struct {
+		a      int
+		b      int
+		expect int
+	}{
+		"1": {
+			a:      3,
+			b:      3,
+			expect: 1,
+		},
+		"2": {
+			a:      8,
+			b:      4,
+			expect: 1, // 2 is correct
+		}}
+
+	for c, tt := range tests {
+		t.Run(c, func(t *testing.T) {
+			if e.Div(tt.a, tt.b) != tt.expect {
+				t.Errorf("add:%d expect:%d", e.Add(tt.a, tt.b), tt.expect)
+			}
+		})
+	}
+}
